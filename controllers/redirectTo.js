0deb5c3238fcfd697ad0ce6477963d71420ef848
abusedmedia <hello@fabiofranchino.com>
2021-03-05 15:02:07 +0100

    added aCircle block

--- controllers/redirectTo.js
+++ controllers/redirectTo.js
@@ -1,3 +1,7 @@
+/*
+This Controller plugin adds a redirectTo function when the user reaches the end of the presentation.
+It exploits the 'end' event of the internal Router to do that.
+*/
 const redirectTo = function (rootElement, router, ctrlConfig, projectConfig) {
     router.on('end', e => {
         location.href = ctrlConfig
