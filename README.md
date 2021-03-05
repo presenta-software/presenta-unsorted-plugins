281b128acd50a6a20f2635d9e1d0fda3c77ba107
abusedmedia <hello@fabiofranchino.com>
2021-03-05 20:01:53 +0100

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# PRESENTA Unsorted Plugins
+
+This repository contains Proof-Of-Concept and experimental plugins for **PRESENTA Lib**.
+
+The main purpose is to test new ideas quickly leaving them available pubblicly for easy evaluation.
+
+For that reason, the whole repository is published under this base URL:
+
+```
+https://unsorted-presenta-plugins.netlify.app/
+```
+
+This means that you can include one or more plugins both in [Playground](https://play.presenta.cc/) tool or within any online coding tool such as [Codepen](https://codepen.io/).
+
+**A word of caution:**
+
+All these plugins have to be considered experimental or proof-of-concept, even if they looks finalized.
+
+A well-structure plugin usually lives in its own repository, ideally alongside a build toolchain, in order to publish a single javscript file that might include external dependencies.
---
+++ modules/randomBubbles.js
@@ -0,0 +1,65 @@
+/*
+This Module plugin adds random visual bubbles to each scene.
+It creates an SVG canvas and add circles in it.
+Colors are inherited by the choosen scheme colors of the presentation.
+Possible settings are: 
+- number of circles to be used
+- color or array of colors to be used
+- opacity or opacity range to be randomized
+*/
+function randomBubbles(sceneElement, modConfig, sceneConfig){
+
+    // in case a specific scene doesn't want this effect
+    if(!modConfig) return
+
+    // options
+    var num = 10
+    var col = 'var(--colorFore)'
+    var opc = 1
+
+    switch(typeof modConfig){
+        case 'number':
+            num = modConfig
+            break
+
+        case 'object':
+            num = modConfig.amount || num
+            col = modConfig.color || col
+            opc = modConfig.opacity || opc
+            break
+    }
+
+    // create the data source for the circles generation
+    const list = []
+    for(var i = 0; i < num; ++i){
+        const r = Math.random()*5
+        const x = Math.random()*100
+        const y = Math.random()*100
+        const o = Array.isArray(opc) ? opc[0] + Math.random() * (opc[1]-opc[0]) : opc
+        var c = col
+        if(Array.isArray(col)){
+            const idx = parseInt(Math.random()*col.length)
+            c = col[idx]
+        }
+        list.push(`<circle opacity="${o}" fill="${c}" r="${r}" cx="${x}" cy="${y}"></circle>`)
+    }
+
+    // create the svg fragment
+    const parser = new DOMParser()
+    const str = `<svg style="width:100%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        ${list.map(d => (d))}
+    </svg>`
+
+    const frag = parser.parseFromString(str, 'text/html').body.childNodes[0]
+    const front = sceneElement.querySelector('.frontContainer')
+    front.append(frag)
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+    })
+
+}
+
+Presenta.addModule('randomBubbles', randomBubbles)
