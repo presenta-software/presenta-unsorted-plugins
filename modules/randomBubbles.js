319bfc9d2bd463f8714c07ecee6544b57409dd6d
abusedmedia <hello@fabiofranchino.com>
2021-03-06 08:42:10 +0100

    some cleanup on randomBubbles

--- modules/randomBubbles.js
+++ modules/randomBubbles.js
@@ -3,45 +3,46 @@ This Module plugin adds random visual bubbles to each scene.
 It creates an SVG canvas and add circles in it.
 Colors are inherited by the choosen scheme colors of the presentation.
 Possible settings are: 
-- number of circles to be used
-- color or array of colors to be used
+- amount of circles to be used
+- color or array of colors to be used, picked randomly
 - opacity or opacity range to be randomized
+- size or size range to be randomized
 */
 function randomBubbles(sceneElement, modConfig, sceneConfig){
 
     // in case a specific scene doesn't want this effect
     if(!modConfig) return
 
-    // options
-    var num = 10
-    var col = 'var(--colorFore)'
-    var opc = 1
+    // handle the options and defaults
+    var amount = 10
+    var color = 'var(--colorFore)'
+    var opacity = 1
     var size = [0.5, 5]
 
     switch(typeof modConfig){
         case 'number':
-            num = modConfig
+            amount = modConfig
             break
 
         case 'object':
-            num = modConfig.amount || num
-            col = modConfig.color || col
-            opc = modConfig.opacity || opc
+            amount = modConfig.amount || amount
+            color = modConfig.color || color
+            opacity = modConfig.opacity || opacity
             size = modConfig.size || size
             break
     }
 
     // create the data source for the circles generation
     const list = []
-    for(var i = 0; i < num; ++i){
+    for(var i = 0; i < amount; ++i){
         const x = Math.random()*100
         const y = Math.random()*100
-        const o = Array.isArray(opc) ? opc[0] + Math.random() * (opc[1]-opc[0]) : opc
+        const o = Array.isArray(opacity) ? opacity[0] + Math.random() * (opacity[1]-opacity[0]) : opacity
         
-        var c = col
-        if(Array.isArray(col)){
-            const idx = parseInt(Math.random()*col.length)
-            c = col[idx]
+        var c = color
+        if(Array.isArray(color)){
+            const idx = parseInt(Math.random()*color.length)
+            c = color[idx]
         }
 
         var r = Array.isArray(size) ? size[0] + Math.random() * (size[1]-size[0]) : size
@@ -49,7 +50,6 @@ function randomBubbles(sceneElement, modConfig, sceneConfig){
         list.push(`<circle opacity="${o}" fill="${c}" r="${r}" cx="${x}" cy="${y}"></circle>`)
     }
 
-    // create the svg fragment
     const parser = new DOMParser()
     const str = `<svg style="width:100%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
         ${list.map(d => (d))}
@@ -57,14 +57,10 @@ function randomBubbles(sceneElement, modConfig, sceneConfig){
 
     const frag = parser.parseFromString(str, 'text/html').body.childNodes[0]
     const front = sceneElement.querySelector('.frontContainer')
+
+    // append to the container
     front.append(frag)
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve()
-        }, 2000)
-    })
-
 }
 
+// register the module with its unique namespace
 Presenta.addModule('randomBubbles', randomBubbles)
